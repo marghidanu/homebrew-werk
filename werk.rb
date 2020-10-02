4ef212e09760d9574d78c13936dd414d7ab29f4c
Tudor Marghidanu <marghidanu@users.noreply.github.com>
2020-10-02 19:41:49 +0300

    Update werk.rb

--- werk.rb
+++ werk.rb
@@ -1,9 +1,9 @@
 class Werk < Formula
     desc "Dead simple task runner"
     homepage "https://itwerks.io"
-    url "https://github.com/marghidanu/werk/releases/download/0.1.1/werk-macos"
-    version "0.1.1"
-    sha256 "f245c4e1b37b4f0c02a98046964376089f225dedcf27a279894c359142774d60"
+    url "https://github.com/marghidanu/werk/releases/download/0.1.2/werk-macos"
+    version "0.1.2"
+    sha256 "64ae99872eb30ec69920030dcbc116f838110373873b07e2e08a7905a010cf16"
     license "MIT"
     
     depends_on "libyaml"
