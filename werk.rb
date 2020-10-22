6f943fbb2229390b3395031a2336c3ff2fb63fba
Tudor Marghidanu <marghidanu@users.noreply.github.com>
2020-10-22 10:35:58 +0300

    Update werk.rb

--- werk.rb
+++ werk.rb
@@ -2,7 +2,7 @@ class Werk < Formula
     desc "Dead simple task runner"
     homepage "https://itwerks.io"
     url "https://github.com/marghidanu/werk/releases/download/0.1.3/werk-macos"
-    version "0.1."
+    version "0.1.3"
     sha256 "18059e8d7f914755895b00a3c9aa9980cf958468d61367cc6129698407e436ed"
     license "MIT"
     
