a7ab99e5c85c4d858519417ed5c19b6011696703
Tudor Marghidanu <marghidanu@users.noreply.github.com>
2021-01-11 23:59:16 +0200

    werk 0.2.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/werk.rb
+++ Formula/werk.rb
@@ -1,9 +1,9 @@
 class Werk < Formula
     desc "Dead simple task runner"
     homepage "https://itwerks.io"
-    url "https://github.com/marghidanu/werk/releases/download/0.2.0/werk-macos-10.15"
-    version "0.2.0"
-    sha256 "b0157a9044000ad28e6b63bc19e3e3152695d68b1f2061aada24755e6b30c85e"
+    url "https://github.com/marghidanu/werk/releases/download/0.2.1/werk-macos-10.15"
+    version "0.2.1"
+    sha256 "17db281343d9fa476cef66002e67afdb126ad0fc9ed0238136143082e58b669a"
     license "MIT"
     
     depends_on "libyaml"
