f1b0a6ef45b50873e5869cd8f9c048c263996d11
Tudor Marghidanu <marghidanu@users.noreply.github.com>
2021-02-18 14:20:13 +0200

    werk 0.4.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/werk.rb
+++ Formula/werk.rb
@@ -1,9 +1,9 @@
 class Werk < Formula
     desc "Dead simple task runner"
     homepage "https://itwerks.io"
-    url "https://github.com/marghidanu/werk/releases/download/0.4.2/werk-macOS"
-    version "0.4.2"
-    sha256 "b3816ec77ee80d933657f4d411d6705329f596d57540786daa44535f72d91553"
+    url "https://github.com/marghidanu/werk/releases/download/0.4.3/werk-macOS"
+    version "0.4.3"
+    sha256 "f34671b89a5b8a24ff7aef0c096393f107f370e489b90de040ce86f2b5d00dd5"
     license "MIT"
     
     depends_on "libyaml"
