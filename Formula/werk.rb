1fbef8031f8972a9ba1350e6adb56ea67cb5f75d
Tudor Marghidanu <marghidanu@users.noreply.github.com>
2021-12-22 18:50:58 +0200

    werk 0.7.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/werk.rb
+++ Formula/werk.rb
@@ -1,9 +1,9 @@
 class Werk < Formula
     desc "Dead simple task runner"
     homepage "https://itwerks.io"
-    url "https://github.com/marghidanu/werk/archive/0.7.0.tar.gz"
-    version "0.7.0"
-    sha256 "01779e7744a59a461a434595f6c2edcf356866d1730163d24cedde650f5861c4"
+    url "https://github.com/marghidanu/werk/archive/0.7.1.tar.gz"
+    version "0.7.1"
+    sha256 "4fa5b63b63721eb848b960bf56283024df6dfc8a2ce7e67b45d2e0b9698ccca1"
     license "MIT"
     
     depends_on "crystal" => :build
