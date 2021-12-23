e95ad769841f39acefe38411bff6f7d7881e3607
Tudor <tudor@marghidanu.com>
2021-12-23 13:08:25 +0200

    Updating version before installation

--- Formula/werk.rb
+++ Formula/werk.rb
@@ -5,10 +5,15 @@ class Werk < Formula
     version "0.7.1"
     sha256 "4fa5b63b63721eb848b960bf56283024df6dfc8a2ce7e67b45d2e0b9698ccca1"
     license "MIT"
-    
+
     depends_on "crystal" => :build
 
     def install
+        ["src/version.cr", "shard.yml"].each do |file|
+            data = File.read(file).gsub("0.0.0", version)
+            File.open(file, "w") { |f| f.write(data) }
+        end
+
         system "shards", "build", "--release", "--no-debug"
         bin.install "bin/werk" => "werk"
     end
